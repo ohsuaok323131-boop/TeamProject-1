01ef6af13e40a08fe7da297a3691ebfd7f02b533
박현서 <h2kgj4gg@gmail.com>
2026-05-14 21:11:54 +0900

    .

---
+++ Student.java
@@ -0,0 +1,106 @@
+
+/**
+ * Student 클래스의 설명을 작성하세요.
+ *
+ * @author (작성자 이름)
+ * @version (버전 번호 또는 작성한 날짜)
+ */
+public class Student{
+    private String studentId;
+    private String name;
+    private String department;
+    private int year;
+
+    private String[] subjectName = new String[6];
+    private String[] itemName = new String[4];
+
+    private double[] maxScore = {100, 100, 100, 100};
+    private double[] weight = {30, 40, 20, 10};
+
+    private double[][] scores = new double[6][4];
+
+    private int subjectCount = 0;
+    private int itemCount = 0;
+
+    public static final int MAX_STUDENTS = 100;
+
+    
+    public Student(String id, String name, String department, int year){
+        this.studentId = id;
+        this.name = name;
+        this.department = department;
+        this.year = year;
+    }
+
+    
+    public void addSubject(String subjectName){
+        if (subjectCount < 6){
+            this.subjectName[subjectCount] = subjectName;
+            subjectCount++;
+        } else{
+            System.out.println("과목은 최대 6개까지 등록 가능합니다.");
+        }
+    }
+
+    
+    public void addItem(String itemName, double max, double weight){
+        if (itemCount < 4){
+            this.itemName[itemCount] = itemName;
+            this.maxScore[itemCount] = max;
+            this.weight[itemCount] = weight;
+            itemCount++;
+        } else{
+            System.out.println("항목은 최대 4개까지 등록 가능합니다.");
+        }
+    }
+
+    
+    public void setScore(int subjectIdx, int itemIdx, double score){
+        if (checkScore(score, maxScore[itemIdx])){
+            scores[subjectIdx][itemIdx] = score;
+        } else{
+            System.out.println("경고: 점수가 범위를 벗어났습니다. 0으로 저장합니다.");
+            scores[subjectIdx][itemIdx] = 0;
+        }
+    }
+
+    
+    public boolean checkScore(double score, double max){
+        return score >= 0 && score <= max;
+    }
+
+    
+    public String getId(){return studentId;}
+    public String getName(){return name;}
+    public String[] getSubjectName(){return subjectName;}
+    public double[][] getScores(){return scores;}
+    public double[] getWeight(){return weight;}
+    public int getSubjectCount(){return subjectCount;}
+    public int getItemCount(){return itemCount;}
+
+    
+    public void showStudent(){
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+        System.out.println("학번: " + studentId);
+        System.out.println("이름: " + name);
+        System.out.println("학과: " + department);
+        System.out.println("학년: " + year);
+        System.out.println("..............................................");
+
+        System.out.print("과목");
+        for (int item = 0; item < itemCount; item++){
+            System.out.print(itemName[item]);
+        }
+        System.out.println();
+
+        for (int s = 0; s < subjectCount; s++){
+            System.out.print(subjectName[s]);
+            for (int item = 0; item < itemCount; item++){
+                System.out.print(scores[s][item]);
+            }
+            System.out.println();
+        }
+
+        System.out.println("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ");
+    }
+}
